12da20a34e88a615002e76d35e2bc499eb489c0a
AnaRibeiroPT <anaribeiro924@gmail.com>
2023-10-31 20:59:41 +0000

    shuffled fruits

--- js/index.js
+++ js/index.js
@@ -1,25 +1,66 @@
 class Game {
-  constructor() {
-    this.height = 15;
-    this.width = 8;
-    this.positionX = 0;
-    this.positionY = 0;
-    this.elements = [];
-  }  
-  createFruits() {
-    this.elements = document.createElement("div");
-    this.elements.className = "elements";
-
-    this.elements.style.width = this.width + "vw";
-    this.elements.style.height = this.height + "vh";
-    this.elements.style.left = this.positionX + "vw";
-    this.elements.style.bottom = this.positionY + "vh";
-
-    const containerDiv = document.querySelector(".container-div");
-    containerDiv.appendChild(this.elements);
+  constructor(fruits) {
+    this.fruits = fruits;
   }
-  moveDown() {
-    this.positionY--;
-    this.elements.style.bottom = this.positionY + "vh";
+  createFruits(columns, rows) {
+    let html = "";
+    let fruitIndex = 0;
+
+    columns.forEach(col => {
+      rows.forEach(row => {
+        const pic = this.fruits[fruitIndex];
+        fruitIndex++;
+
+        if (pic) {
+          html += `
+          <div class="fruits" row-and-column="${col}${row}">
+          <div id="container-background"></div>
+          <img draggable="false" class="fruitsImg" fruits-name="${pic.name}" src="./images/${pic.img}">
+          </div>`;
+        }
+        const containerDiv = document.querySelector("#container-div");
+        if (containerDiv) {
+          containerDiv.innerHTML = html;
+        } else {
+          console.error("#container-div not found in the HTML.");
+        }
+      });
+    });
+    document.querySelector("#container-div").innerHTML = html;
+  }
+
+  swapFruits(fruit1, fruit2) {
+    const position1 = getRowAndColumn(fruit1);
+    const position2 = getRowAndColumn(fruit2);
+
+    const columnDifference = position1.column.charCodeAt(0) - position2.column.charCodeAt(0);
+    const rowDifference = position1.row - position2.row;
+
+    if (
+      (columnDifference === 1 || columnDifference === -1) && rowDifference === 0 ||
+      (rowDifference === 1 || rowDifference === -1) && columnDifference === 0
+    ) {
+      const swapping = fruit1.innerHTML;
+      fruit1.innerHTML = fruit2.innerHTML;
+      fruit2.innerHTML = swapping;
+    }
+  }
+  shuffleFruits() {
+    for (let i = this.fruits.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = this.fruits[i];
+      this.fruits[i] = this.fruits[j];
+      this.fruits[j] = temp;
+    }
+    const containerDiv = document.querySelector("#container-div");
+    containerDiv.innerHTML = "";
   }
 }
+
+function getRowAndColumn(fruit) {
+  const rowAndCol = fruit.getAttribute("row-and-column");
+  const column = rowAndCol.charAt(0);
+  const row = parseInt(rowAndCol.slice(1));
+  return { column, row };
+}
+
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const fruits = [
+const fruitsData = [
   { name: "strawberry", img: "strawberry.png" },
   { name: "grapes", img: "grapes.png" },
   { name: "lemon", img: "lemon.png" },
@@ -25,75 +25,28 @@ const fruits = [
   { name: "orange", img: "orange.png" },
 ]
 
-
-/* this.fruitsArr = [];
-const newElement = new Game(); 
-
-window.addEventListener('load', (event) => {
-  //gonna add this later
-  setInterval(() => {
-    fruitsArr.forEach((fruit) => {
-      fruit.moveDown();
-    });
-  }, 50); 
-}); 
- */
-
 const columns = ["A", "B", "C", "D"];
-const rows = [1, 2, 3, 4, 5, 6,];
+const rows = [1, 2, 3, 4, 5, 6];
 
-let html = "";
-let fruitIndex = 0;
 
-columns.forEach(col => {
-  rows.forEach(row => {
-    const pic = fruits[fruitIndex];
-    fruitIndex++;
+const game = new Game(fruitsData);
+game.shuffleFruits();
+game.createFruits(columns, rows);
 
-    if (pic) {
-      html += `
-    <div class="fruits" row-and-column="${col}${row}">
-    <div id="container-background"></div>
-    <img draggable="false" class="fruitsImg" fruits-name="${pic.name}" src="./images/${pic.img}">
-    </div>`;
-    }
-  });
-});
-
-document.querySelector("#container-div").innerHTML = html;
-
-function getRowAndColumn(fruit) {
-  const rowAndCol = fruit.getAttribute("row-and-column");
-  const column = rowAndCol.charAt(0);
-  const row = parseInt(rowAndCol.slice(1));
-  return { column, row };
-}
 
 let firstFruit = null;
 document.querySelectorAll(".fruits").forEach((fruit) => {
   fruit.addEventListener("click", () => {
     if (firstFruit === null) {
+      fruit.querySelector(".fruitsImg").classList.add("enlarged");
       firstFruit = fruit;
     } else {
-      swapFruits(firstFruit, fruit);
+      game.swapFruits(firstFruit, fruit);
       firstFruit = null;
+
+      document.querySelectorAll(".fruits").forEach((fruit) => {
+        fruit.querySelector(".fruitsImg").classList.remove("enlarged");
+      });
     }
   });
 });
-
-function swapFruits(fruit1, fruit2) {
-  const position1 = getRowAndColumn(fruit1);
-  const position2 = getRowAndColumn(fruit2);
-
-  const columnDifference = position1.column.charCodeAt(0) - position2.column.charCodeAt(0);
-  const rowDifference = position1.row - position2.row;
-
-  if (
-    (columnDifference === 1 || columnDifference === -1) && rowDifference === 0 || 
-    (rowDifference === 1 || rowDifference === -1) && columnDifference === 0
-  ) {
-    const swapping = fruit1.innerHTML;
-    fruit1.innerHTML = fruit2.innerHTML;
-    fruit2.innerHTML = swapping;
-  }
-}
